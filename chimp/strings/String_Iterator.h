8eb487c8cafd662bcf6851dbe3d81d1865489ebf
santerijps <santeri.sydanmetsa@protonmail.com>
2025-04-11 17:48:11 +0300

    rename Byte_Iterator to String_Iterator

--- chimp/strings/String_Iterator.h
+++ chimp/strings/String_Iterator.h
@@ -1,39 +1,39 @@
-#ifndef LIBCHIMP_BYTE_ITERATOR_H
-#define LIBCHIMP_BYTE_ITERATOR_H
+#ifndef LIBCHIMP_STRING_ITERATOR_H
+#define LIBCHIMP_STRING_ITERATOR_H
 
 #include <assert.h>
 #include <stdint.h>
 
-typedef struct Byte_Iterator_Position Byte_Iterator_Position;
-struct Byte_Iterator_Position {
+typedef struct String_Iterator_Position String_Iterator_Position;
+struct String_Iterator_Position {
     uint64_t offset;
     uint64_t line;
     uint64_t column;
 };
 
-typedef struct Byte_Iterator Byte_Iterator;
-struct Byte_Iterator {
+typedef struct String_Iterator String_Iterator;
+struct String_Iterator {
     char* const string;
     size_t const length;
     uint64_t offset;
-    Byte_Iterator_Position position;
+    String_Iterator_Position position;
 };
 
-typedef struct Byte_Iterator_Result Byte_Iterator_Result;
-struct Byte_Iterator_Result {
-    Byte_Iterator_Position position;
+typedef struct String_Iterator_Result String_Iterator_Result;
+struct String_Iterator_Result {
+    String_Iterator_Position position;
     int byte;
 };
 
 // Create a byte iterator.
 __attribute__((warn_unused_result))
-Byte_Iterator byte_iterator_create(
+String_Iterator string_iterator_create(
     char* const string,
     size_t const length
 ) {
     assert(string != NULL);
     assert(length > 0);
-    return (Byte_Iterator) {
+    return (String_Iterator) {
         .string = string,
         .length = length,
         .offset = 0,
@@ -43,13 +43,13 @@ Byte_Iterator byte_iterator_create(
 
 // Read the next byte and increment the offset.
 __attribute__((warn_unused_result))
-Byte_Iterator_Result byte_iterator_next(Byte_Iterator* const iter) {
+String_Iterator_Result string_iterator_next(String_Iterator* const iter) {
     assert(iter != NULL);
     assert(iter->string != NULL);
     assert(iter->length > 0);
     assert(iter->offset <= iter->length);
 
-    Byte_Iterator_Result result = {
+    String_Iterator_Result result = {
         .position = iter->position,
         .byte = 0,
     };
@@ -72,13 +72,13 @@ Byte_Iterator_Result byte_iterator_next(Byte_Iterator* const iter) {
 
 // Read the next byte but DO NOT increment the offset.
 __attribute__((warn_unused_result))
-Byte_Iterator_Result byte_iterator_peek(const Byte_Iterator* const iter) {
+String_Iterator_Result string_iterator_peek(const String_Iterator* const iter) {
     assert(iter != NULL);
     assert(iter->string != NULL);
     assert(iter->length > 0);
     assert(iter->offset <= iter->length);
 
-    Byte_Iterator_Result result = {
+    String_Iterator_Result result = {
         .position = iter->position,
         .byte = 0,
     };
