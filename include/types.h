7b337f005dc5c47977e8da93a504fdb763ac740d
santerijps <santeri.sydanmetsa@protonmail.com>
2025-04-11 13:19:45 +0300

    remove stdint.h dependency from types.h

--- include/types.h
+++ include/types.h
@@ -1,17 +1,15 @@
 #ifndef LIBCHIMP_TYPES_H
 #define LIBCHIMP_TYPES_H
 
-#include <stdint.h>
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long long i64;
 
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
-
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
 
 typedef float f32;
 typedef double f64;
